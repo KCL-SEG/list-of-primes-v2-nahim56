9c7c5c8044bdc1edb9bbbbd136b8bb447135649e
Nahimul Hussein <nahimul.hussein@kcl.ac.uk>
2022-10-01 16:46:02 +0100

    solution

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1 :
+        raise ValueError
     list = []
+    x = 2
+    
+    while number_of_primes!=0:
+     for i in range (2,x):
+        if x%i == 0:
+            break
+        else:
+         list.append(x)
+         number_of_primes-=1
+         x += 1
     return list
